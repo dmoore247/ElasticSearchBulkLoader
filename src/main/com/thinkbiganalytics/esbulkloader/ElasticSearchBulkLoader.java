9a0fc4e048c3f42b81d053869bfaa0cb80bf40b3
Douglas Moore <Douglas.Moore@ThinkBigAnalytics.com>
2013-10-06 17:11:41 -0400

    Add input paths and arg processing

--- src/main/com/thinkbiganalytics/esbulkloader/ElasticSearchBulkLoader.java
+++ src/main/com/thinkbiganalytics/esbulkloader/ElasticSearchBulkLoader.java
@@ -2,18 +2,28 @@ package com.thinkbiganalytics.esbulkloader;
 
 import java.io.IOException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class ElasticSearchBulkLoader {
+    
+    private static Log log = LogFactory.getLog(ElasticSearchBulkLoader.class);
     public static void main(String args[]) throws Exception {
+	
+	log.info(String.format("%s %s %s", args[0], args[1], args[2]));
+	
 	Configuration conf = new Configuration();
 
-	Job job = new Job(conf, "add_files_to_es");
+	Job job = new Job(conf, args[0]);
 	job.setJarByClass(ElasticSearchBulkLoader.class);
 	job.setMapperClass(IndexFilesMapper.class);
 	job.setNumReduceTasks(0);
@@ -21,6 +31,9 @@ public class ElasticSearchBulkLoader {
 	job.setMapOutputValueClass(Text.class);
 	job.setInputFormatClass(TextInputFormat.class);
 	job.setOutputFormatClass(ElasticsearchBulkFormat.class);
+	
+	 FileInputFormat.addInputPath(job, new Path(args[1]));
+	 FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
 	// actually run the job
 	boolean success = job.waitForCompletion(true);
