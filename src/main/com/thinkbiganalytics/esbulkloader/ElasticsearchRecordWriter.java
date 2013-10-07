e93d7bed5f4edd9d2f0d66b1a54d352a79bea02c
Douglas Moore <Douglas.Moore@ThinkBigAnalytics.com>
2013-10-07 02:35:05 -0400

    add flushing logic

--- src/main/com/thinkbiganalytics/esbulkloader/ElasticsearchRecordWriter.java
+++ src/main/com/thinkbiganalytics/esbulkloader/ElasticsearchRecordWriter.java
@@ -3,6 +3,7 @@ package com.thinkbiganalytics.esbulkloader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
@@ -19,6 +20,8 @@ import org.apache.hadoop.mapreduce.TaskAttemptContext;
  * 
  */
 public class ElasticsearchRecordWriter extends RecordWriter<Text, Text> {
+    private static final int DEFAULT_BUFFER_SIZE = 100000;
+    private static final String ESBL_BUFFER_SIZE = "esbl.buffer_size";
     private static final String DEFAULT_ES_PORT = "9200";
     private static final String DEFAULT_ES_HOST = "localhost";
     private static final String ESBL_PORT = "esbl.port";
@@ -26,14 +29,19 @@ public class ElasticsearchRecordWriter extends RecordWriter<Text, Text> {
 
     private static Log log = LogFactory.getLog(ElasticsearchRecordWriter.class);
 
-    PostMethod method;
-    final String apiUrl;
+    private PostMethod method;
+    private final String apiUrl;
+    private final int bufferSize;
+    private final StringBuffer buffer ;
 
     public ElasticsearchRecordWriter(TaskAttemptContext context) {
 	String host = context.getConfiguration().get(ESBL_HOST);
 	String port = context.getConfiguration().get(ESBL_PORT);
+	String size = context.getConfiguration().get(ESBL_BUFFER_SIZE);
 	host = (null == host ? DEFAULT_ES_HOST : host);
 	port = (null == port ? DEFAULT_ES_PORT : port);
+	bufferSize = (null == size ? DEFAULT_BUFFER_SIZE : Integer.parseInt(size));
+	buffer = new StringBuffer(bufferSize);
 	apiUrl = String.format("http://%s:%s/_bulk", host, port);
     }
 
@@ -41,9 +49,29 @@ public class ElasticsearchRecordWriter extends RecordWriter<Text, Text> {
     public void write(Text key, Text value) throws IOException,
 	    InterruptedException {
 
+	buffer.append(value.toString());
 	log.info(value.toString());
+	
+	checkFlush();
+    }
+
+    @Override
+    public void close(TaskAttemptContext arg0) throws IOException,
+	    InterruptedException {
+	flush();
+	method.releaseConnection();
+    }
+
+    private void checkFlush() throws UnsupportedEncodingException {
+	if (buffer.length()>this.bufferSize) {
+	    flush();
+	    buffer.setLength(0);
+	}
+    }
+    
+    private void flush() throws UnsupportedEncodingException {
 	method = new PostMethod(apiUrl);
-	method.setRequestEntity(new StringRequestEntity(value.toString(),
+	method.setRequestEntity(new StringRequestEntity(buffer.toString(),
 		"text/json", "UTF-8"));
 
 	BufferedReader br = null;
@@ -78,12 +106,6 @@ public class ElasticsearchRecordWriter extends RecordWriter<Text, Text> {
 		}
 	    }
 	}
-    }
-
-    @Override
-    public void close(TaskAttemptContext arg0) throws IOException,
-	    InterruptedException {
-	method.releaseConnection();
     }
 
 }
